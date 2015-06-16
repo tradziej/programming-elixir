bbc615705088d44b3eca1b634fa3d65517cdd91c
Tomasz Radziejewski <tomasz@radziejewski.pl>
2015-06-16 17:38:44 +0200

    Map function

--- lists/mylist1.exs
+++ lists/mylist1.exs
@@ -6,7 +6,12 @@ defmodule MyList do
   def add_1([]), do: []
 
   def add_1([head | tail]), do: [head + 1 | add_1(tail)]
+
+  def map([], _func), do: []
+
+  def map([head | tail], func), do: [func.(head) | map(tail, func)]
 end
 
 IO.inspect MyList.square([5, 6, 7])
 IO.inspect MyList.add_1([5, 6, 7])
+IO.inspect MyList.map([5, 6, 7], &(&1 * &1))
